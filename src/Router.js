e56c3a6451f0b727ceb23cb8759f56c5172f2638
HeinLee <heinlee4680@gmail.com>
2023-03-15 14:23:05 +0900

    ADD:초기세팅

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default Router;
