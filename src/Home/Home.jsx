cefcd64af8e7010fc178f66bbb1043581b92f0c1
macbook <macbook@jungee.local>
2023-03-20 13:43:13 +0900

    Add: Home에 지도 추가

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import {
+  Container as MapDiv,
+  NaverMap,
+  Marker,
+  useNavermaps,
+} from 'react-naver-maps';
+import * as S from './Home.style';
 
 const Home = () => {
-  useEffect(()=>{
-    if (인터넷연결 확인 = true)
-    {Navigate("스플래시 링크")}
-    else {
-      alert("문제 발생")
-      앱종료되는 로직
-    }
-  },[])
-  return <div>123</div>;
+  // useEffect(()=>{
+  //   if (인터넷연결 확인 = true)
+  //   {Navigate("스플래시 링크")}
+  //   else {
+  //     alert("문제 발생")
+  //     앱종료되는 로직
+  //   }
+  // },[])
+  const navermaps = useNavermaps();
+
+  return (
+    <S.MapBox>
+      <NaverMap
+        defaultCenter={new navermaps.LatLng(37.5568085, 126.9199839)}
+        defaultZoom={15}
+        zoomControl={true}
+      >
+        <Marker position={new navermaps.LatLng(37.5568085, 126.9199839)} />
+      </NaverMap>
+    </S.MapBox>
+  );
 };
 
 export default Home;
--- src/Home/Home.style.js
+++ src/Home/Home.style.js
@@ -1,1 +1,12 @@
+import styled from 'styled-components';
+import {
+  Container as MapDiv,
+  NaverMap,
+  Marker,
+  useNavermaps,
+} from 'react-naver-maps';
 
+export const MapBox = styled(MapDiv)`
+  height: 640px;
+  width: 360px;
+`;
