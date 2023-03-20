7ea48713a3eae3bc5b9c43786ed3452099c17611
HeinLee <heinlee4680@gmail.com>
2023-03-20 10:58:44 +0900

    초기세팅들

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,16 @@
-import React from "react";
-import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home/Home';
+import Login from './Login/Login';
+import Signup from './Signup/Signup';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
