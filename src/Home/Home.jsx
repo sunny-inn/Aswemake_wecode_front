09300c1aadb96661821a79a64af3134453de1842
HeinLee <heinlee4680@gmail.com>
2023-03-20 14:05:16 +0900

    :ADD:LOGIN

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 
 const Home = () => {
-  useEffect(()=>{
-    if (인터넷연결 확인 = true)
-    {Navigate("스플래시 링크")}
-    else {
-      alert("문제 발생")
-      앱종료되는 로직
-    }
-  },[])
+  // useEffect(()=>{
+  //   if (인터넷연결 확인 = true)
+  //   {Navigate("스플래시 링크")}
+  //   else {
+  //     alert("문제 발생")
+  //     앱종료되는 로직
+  //   }
+  // },[])
   return <div>123</div>;
 };
 
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>로그인</h1>
+      <input name="id" type="text" placeholder="아이디" />
+      <input name="pw" type="password" placeholder="비밀번호" />
+      <div>
+        <input type="checkbox" />
+        <label>로그인 상태 유지</label>
+      </div>
+      <button>
+        <span>로그인</span>
+      </button>
+      <p>아직 회원이 아니신가요?</p>
+    </div>
+  );
 };
 
 export default Login;
