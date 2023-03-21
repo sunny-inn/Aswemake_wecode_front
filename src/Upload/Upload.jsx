04a70b33035c2018ebdc03f89e51b5fb8ab96403
macbook <macbook@jungee.local>
2023-03-21 18:02:28 +0900

    -upload 구현 중

--- src/Upload/Upload.jsx
+++ src/Upload/Upload.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Upload = () => {
-  return <div></div>;
+  return <div>
+    
+  </div>;
 };
 export default Upload;
---
+++ src/Upload/Upload.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const UploadBox = styled.form`
+  height: 640px;
+  width: 360px;
+  border: 1px solid black;
+`;
