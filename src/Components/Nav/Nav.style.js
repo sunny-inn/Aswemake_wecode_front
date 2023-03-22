5c535b842bee0e0cc9fa0126068e4fe928983624
HeinLee <heinlee4680@gmail.com>
2023-03-22 19:07:30 +0900

    nav추가다시

---
+++ src/Components/Nav/Nav.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const NavBox = styled.div`
+  position: absolute;
+  width: 359px;
+  height: 42px;
+  left: 0px;
+  top: 0px;
+  background: #d9d9d9;
+  z-index: 10;
+`;
