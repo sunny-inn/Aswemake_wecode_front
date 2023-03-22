feff9aecf5ebb6a5ea2a5d14428d96cde68c602d
macbook <macbook@jungee.local>
2023-03-22 18:04:29 +0900

    confilct 해결 -회원가입 완료 시 모달창 구현 중

--- src/Signup/Signup.jsx
+++ src/Signup/Signup.jsx
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import DaumPostcode, { useDaumPostcodePopup } from 'react-daum-postcode';
-import * as S from './Signup.style';
-
-const Signup = () => {
-  const [openPostcode, setOpenPostcode] = useState(false);
-
-=======
 import React, { useState, useEffect } from 'react';
 import { useDaumPostcodePopup } from 'react-daum-postcode';
-import Address from './SignupComponents/Address';
+import Modal from '../Components/Modal/Modal';
 import * as S from './Signup.style';
 
 const Signup = () => {
->>>>>>> 3546a62 (Add: 주소 찾기 기능 구현 중)
   const [signupInfo, setSignupInfo] = useState({
     id: '',
     passwd: '',
@@ -26,11 +16,8 @@ const Signup = () => {
     gender: '',
   });
 
-<<<<<<< HEAD
-=======
   const [isModalOpen, setIsModalOpen] = useState(false);
 
->>>>>>> 3546a62 (Add: 주소 찾기 기능 구현 중)
   const {
     id,
     passwd,
@@ -46,11 +33,8 @@ const Signup = () => {
     setSignupInfo(prev => ({ ...prev, id: e.target.value }));
   };
 
-<<<<<<< HEAD
-=======
   const handleModal = () => setIsModalOpen(true);
 
->>>>>>> 3546a62 (Add: 주소 찾기 기능 구현 중)
   const handlePasswd = e => {
     setSignupInfo(prev => ({ ...prev, passwd: e.target.value }));
   };
@@ -105,80 +89,28 @@ const Signup = () => {
     setSignupInfo(prev => ({ ...prev, gender: e.target.value }));
   };
 
-<<<<<<< HEAD
-  return (
-    <S.SignupBox>
-      {!openPostcode ? (
-        <>
-          <label>아이디</label>
-          <div>
-            <input name="id" value={id} type="text" onChange={handleId} />
-            <button>중복 확인</button>
-          </div>
-          <label>비밀번호</label>
-          <input
-            name="passwd"
-            value={passwd}
-            type="password"
-            onChange={handlePasswd}
-          />
-          <label>비밀번호 확인</label>
-          <input
-            name="passwdCheck"
-            value={passwdCheck}
-            type="password"
-            onChange={handlePasswdCheck}
-          />
-          {passwdCheck !== '' && passwd !== passwdCheck && (
-            <div>비밀번호가 다릅니다</div>
-          )}
-          <label>휴대폰 번호</label>
-          <input
-            name="phoneNumber"
-            value={phoneNumber}
-            type="text"
-            onChange={handlePhoneNumber}
-          />
-          {!address ? (
-            <>
-              <label>주소 찾기</label>
-              <button type="button" onClick={handleClick}>
-                주소 검색
-              </button>
-            </>
-          ) : (
-            <>
-              <label>주소</label>
-              <input
-                name="postalCode"
-                value={postalCode}
-                onChange={handleComplete}
-              />
-              <input name="address" value={address} onChange={handleComplete} />
-=======
-  // 회원가입 완료 버튼
-  // const onSubmit = e => {
-  //   e.preventDefault();
-
-  //   if (payments.totalProductPrice && payments.totalDeliveryPrice) {
-  //     fetch(`${API.ORDERS}/payment`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json;charset=utf-8',
-  //         authorization: Token,
-  //       },
-  //       body: JSON.stringify({ payments }),
-  //     })
-  //       .then(res => res.json())
-  //       .then(data => {
-  //         if (data.message === '') {
-  //           navigate('/');
-  //         } else {
-  //           alert('실패');
-  //         }
-  //       });
-  //   }
-  // };
+  const onSubmit = e => {
+    e.preventDefault();
+    setIsModalOpen(prev => !prev);
+    // if (payments.totalProductPrice && payments.totalDeliveryPrice) {
+    //   fetch(`${API.ORDERS}/payment`, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json;charset=utf-8',
+    //       authorization: Token,
+    //     },
+    //     body: JSON.stringify({ payments }),
+    //   })
+    //     .then(res => res.json())
+    //     .then(data => {
+    //       if (data.message === '') {
+    //         navigate('/');
+    //       } else {
+    //         alert('실패');
+    //       }
+    //     });
+    // }
+  };
 
   return (
     <S.SignupBox>
@@ -216,14 +148,6 @@ const Signup = () => {
         type="text"
         onChange={handlePhoneNumber}
       />
-      {/* <Address
-        postalCode={postalCode}
-        address={address}
-        addressDetail={addressDetail}
-        handleClick={handleClick}
-        handleComplete={handleComplete}
-        handleAddressDetail={handleAddressDetail}
-      /> */}
       <div>
         {!address ? (
           <>
@@ -238,7 +162,6 @@ const Signup = () => {
             <div>
               <input name="postalCode" value={postalCode} readOnly />
               <input name="address" value={address} readOnly />
->>>>>>> 3546a62 (Add: 주소 찾기 기능 구현 중)
               <input
                 name="addressDetail"
                 value={addressDetail}
@@ -246,32 +169,6 @@ const Signup = () => {
                 onChange={handleAddressDetail}
                 placeholder="상세 주소"
               />
-<<<<<<< HEAD
-            </>
-          )}
-          <label>생년월일</label>
-          <input
-            name="birth"
-            value={birth}
-            type="text"
-            onChange={handleBirth}
-          />
-          <label>성별</label>
-          <select name="gender" onChange={e => handleGender(e)}>
-            <option value="none" hidden>
-              성별
-            </option>
-            {GENDER_OPTIONS.map(({ id, gender }) => (
-              <option key={id} value={gender}>
-                {gender}
-              </option>
-            ))}
-          </select>
-        </>
-      ) : (
-        <DaumPostcode onComplete={handleComplete} autoClose />
-      )}
-=======
             </div>
           </>
         )}
@@ -290,9 +187,8 @@ const Signup = () => {
           </option>
         ))}
       </select>
-
->>>>>>> 3546a62 (Add: 주소 찾기 기능 구현 중)
-      <button>가입하기</button>
+      <button onClick={onSubmit}>가입하기</button>
+      {isModalOpen && <Modal />}
     </S.SignupBox>
   );
 };
