5ace469304bb4a2215df743b19a7fbb5d9140fc1
macbook <macbook@jungee.local>
2023-03-24 19:30:57 +0900

    -terms 컴포넌트 적용

--- src/Mypage/Mypage.jsx
+++ src/Mypage/Mypage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Withdraw from './MypageComponents/Withdraw/Withdraw';
 import Switch from './MypageComponents/Switch/Switch';
-import Terms from '../Components/Terms';
+import Terms from '../Components/Terms/Terms';
 import * as S from './Mypage.style';
 
 const Mypage = () => {
@@ -13,7 +13,7 @@ const Mypage = () => {
   const toFlyersList = () => setIsFlyersList(prev => !prev);
   const toWithdraw = () => setIsWithdraw(prev => !prev);
   const toSwitch = () => setIsSwitch(prev => !prev);
-  const toTerms = () => setIsTerms(prev => !prev);
+  const onClickTerms = () => setIsTerms(prev => !prev);
 
   //TODO: token 가져와서 이름이랑 포인트 정보 뿌려주기
 
@@ -28,8 +28,8 @@ const Mypage = () => {
         <button onClick={toSwitch}>계좌 변경</button>
         {isSwitch && <Switch />}
       </div>
-      <button onClick={toTerms}>큐마켓 전단지도 이용약관 확인하기</button>
-      {isTerms && <Terms />}
+      <button onClick={onClickTerms}>큐마켓 전단지도 이용약관 확인하기</button>
+      {isTerms && <Terms onClickTerms={onClickTerms} />}
     </S.MypageBox>
   );
 };
