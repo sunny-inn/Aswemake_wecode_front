0ec9ad8e36735c2b8a651ac0578c3aa0f9829951
HeinLee <heinlee4680@gmail.com>
2023-03-28 19:03:52 +0900

    global적용된다

--- src/Styles/Globalstyle.js
+++ src/Styles/Globalstyle.js
@@ -1,26 +1,38 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
-const GlobalStyle = createGlobalStyle`  
-* {
-  height: 640px;
-  width: 360px;
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
     box-sizing: border-box;
-    font-family: 'Noto Sans KR', sans-serif;  
+    font-family: 'Noto Sans KR', sans-serif;
   }
-a{
+
+  a {
     text-decoration: none;
-    color:black;
+    color: black;
   }
-button{
-      cursor: pointer;
-    }
-li{
-  list-style:none;
-}
-input:focus{
-  outline: none;
-}
+
+  button {
+    cursor: pointer;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  html,
+  body,
+  #root {
+    height: 616px;
+    top:24px;
+    width: 360px;
+  }
 `;
 
 export default GlobalStyle;
