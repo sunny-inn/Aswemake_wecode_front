7436617a0c98bc8fba1451dea07c4ee381f628f1
HeinLee <heinlee4680@gmail.com>
2023-03-31 12:47:57 +0900

    add:캐러셀움직인다git add .
    :wq

--- src/Home/HomeCarousel.jsx
+++ src/Home/HomeCarousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './HomeCarousel.style';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -14,43 +14,52 @@ const HomeCarousel = ({ homeMartList, selectedMart, handleModal }) => {
     centerPadding: '25px',
     dot: false,
   };
-
+  const [slider, setSlider] = useState(null);
   const smIndex = homeMartList.indexOf(selectedMart);
-  const newArray = [
-    ...homeMartList.slice(0, 1),
-    ...homeMartList.slice(1),
-    selectedMart,
-  ];
 
-  if (smIndex > -1) {
-    const newArray = [
-      ...homeMartList.slice(0, smIndex),
-      ...homeMartList.slice(smIndex + 1),
-      selectedMart,
-    ];
-  }
-  const selectedMartList = selectedMart ? newArray : [];
+  // const newArray = [
+  //   ...homeMartList.slice(0, 1),
+  //   ...homeMartList.slice(1),
+  //   selectedMart,
+  // ];
+
+  // if (smIndex > -1) {
+  //   const newArray = [
+  //     ...homeMartList.slice(0, smIndex),
+  //     ...homeMartList.slice(smIndex + 1),
+  //     selectedMart,
+  //   ];
+  // }
+  const selectedMartList = selectedMart ? homeMartList : [];
+
+  useEffect(() => {
+    if (slider) {
+      slider.slickGoTo(smIndex);
+    }
+  }, [smIndex]);
+
   return (
     <S.CarouselWholeContainer>
-      <Slider {...settings}>
-        {selectedMartList.map(martlist => (
-          <S.MartBox key={martlist.id} onClick={handleModal}>
-            <S.CarouselBox>
-              <div>
-                <S.CarouselImg alt="전단지" />
-              </div>
-              <S.CarouselContent>
-                <li>{martlist.name}</li>
-                <li>
-                  <S.StarImg src="./images/starIcon.png" alt="별점" />
-                  <S.RatingSpan>{martlist.ratings} ( )</S.RatingSpan>
-                </li>
-                <li>주소 : {martlist.address}</li>
-                <li>연락처 : {martlist.phoneNumber}</li>
-              </S.CarouselContent>
-            </S.CarouselBox>
-          </S.MartBox>
-        ))}
+      <Slider {...settings} ref={setSlider}>
+        {selectedMartList &&
+          selectedMartList.map(mart => (
+            <S.MartBox key={mart.id} onClick={handleModal}>
+              <S.CarouselBox>
+                <div>
+                  <S.CarouselImg alt="전단지" />
+                </div>
+                <S.CarouselContent>
+                  <li>{mart.name}</li>
+                  <li>
+                    <S.StarImg src="./images/starIcon.png" alt="별점" />
+                    <S.RatingSpan>{mart.ratings}</S.RatingSpan>
+                  </li>
+                  <li>주소 : {mart.address}</li>
+                  <li>연락처 : {mart.phoneNumber}</li>
+                </S.CarouselContent>
+              </S.CarouselBox>
+            </S.MartBox>
+          ))}
       </Slider>
     </S.CarouselWholeContainer>
   );
