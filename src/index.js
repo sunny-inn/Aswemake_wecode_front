7703c8501c648b429c6baed403aea7529684e587
HeinLee <heinlee4680@gmail.com>
2023-04-03 14:57:34 +0900

    merge

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import Globalstyle from './Styles/Globalstyle';
-import theme from './Styles/Theme';
+import theme from './Styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
