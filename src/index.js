5f07cf3ef34b13cde8ea23ed7779cc9b99f9d807
macbook <macbook@jungee.local>
2023-04-04 18:27:22 +0900

    MODIFY: index.js 수정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import Globalstyle from './Styles/Globalstyle';
-import theme from './Styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider>
     <Globalstyle />
     <Router />
   </ThemeProvider>
