07c9ca2127e344aa5f1ef0fc8461193f5cd2e1a9
macbook <macbook@jungee.local>
2023-04-05 18:13:08 +0900

    MODIFY: phone.jsx api 변경

--- src/Signup/SignupComponents/Phone/Phone.jsx
+++ src/Signup/SignupComponents/Phone/Phone.jsx
@@ -29,7 +29,7 @@ const Phone = ({
     setCodeBtn(true);
 
     codeBtn === true &&
-      fetch('http://172.30.1.41:8000/api/verificationCode/send', {
+      fetch('https://flyers.qmarket.me/api/verificationCode/send', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
@@ -58,7 +58,7 @@ const Phone = ({
     e.preventDefault();
 
     code &&
-      fetch('http://172.30.1.41:8000/api/verificationCode/check', {
+      fetch('https://flyers.qmarket.me/api/verificationCode/check', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
