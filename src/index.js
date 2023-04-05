e9ba9ae694a97a938a64471e0f7599a74f943ed0
macbook <macbook@jungee.local>
2023-04-05 11:18:55 +0900

    MODIFY: themeprovider 삭제

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import Globalstyle from './Styles/Globalstyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider>
+  <>
     <Globalstyle />
     <Router />
-  </ThemeProvider>
+  </>
 );
