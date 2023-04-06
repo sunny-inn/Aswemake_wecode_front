bb090f9409e391fb5d80a86fd179f7ec909caef2
eejungee <sulau861@gmail.com>
2023-04-06 16:14:47 +0900

    -login.jsx api 수정

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -69,9 +69,20 @@ const Home = () => {
       },
     })
       .then(response => {
-        const setCookieHeader = response.headers.get('Set-Cookie');
-        console.log(setCookieHeader);
         console.log(document.cookie);
+
+        const parseCookie = str =>
+          str
+            .split(';')
+            .map(v => v.split('='))
+            .reduce((acc, v) => {
+              acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(
+                v[1].trim()
+              );
+              return acc;
+            }, {});
+
+        console.log(parseCookie(document.cookie));
 
         response.json();
       })
