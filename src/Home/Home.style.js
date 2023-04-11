192df26ba7018f3d4b63ef271dbe1b7e619c9770
HeinLee <heinlee4680@gmail.com>
2023-04-11 18:47:24 +0900

    location추가

--- src/Home/Home.style.js
+++ src/Home/Home.style.js
@@ -77,6 +77,6 @@ export const ModalUploadButton = styled.button`
 export const CurrentLocation = styled.img`
   width: 60px;
   height: 60px;
-  bottom: 80px;
+  bottom: 100px;
   position: absolute;
 `;
