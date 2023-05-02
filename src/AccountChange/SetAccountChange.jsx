36b4e97dc3fc100d91a78b734625fa77c9b64be7
minkyung <minngaeng@gmail.com>
2023-05-03 06:33:17 +0900

    ADD: 수정

--- src/AccountChange/SetAccountChange.jsx
+++ src/AccountChange/SetAccountChange.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as S from './SetAccountChange.style';
 import Header from '../Components/Header/Header';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import LoginLayout from '../Login/Component/LoginLayout';
 
 const SetAccountChange = () => {
   const navigate = useNavigate();
@@ -13,16 +14,18 @@ const SetAccountChange = () => {
   };
   return (
     <div>
-      <Header type="withdraw" onClickBack={onClickBack} />
-      <S.Container>
-        <S.Name>{accountHolderName}</S.Name>
-        <span>&nbsp;님</span>
-        <S.Inform>&nbsp;&nbsp;9000원</S.Inform>
-        <S.Inform>&nbsp;&nbsp;인출 요청이 완료되었습니다!</S.Inform>
-      </S.Container>
-      <Link to="/">
-        <S.FinBtn>확인</S.FinBtn>
-      </Link>
+      <Header type="accountChange" onClickBack={onClickBack} />
+      <LoginLayout>
+        <S.Container>
+          <S.Name>{accountHolderName}</S.Name>
+          <span>&nbsp;님</span>
+          <S.Inform>&nbsp;&nbsp;9000원</S.Inform>
+          <S.Inform>&nbsp;&nbsp;인출 요청이 완료되었습니다!</S.Inform>
+        </S.Container>
+        <Link to="/">
+          <S.FinBtn>확인</S.FinBtn>
+        </Link>
+      </LoginLayout>
     </div>
   );
 };
