d4048b050a76ffcdeab6a10cfea3c2b7c11d44a3
HeinLee <heinlee4680@gmail.com>
2023-05-03 16:00:31 +0900

    favorite

--- src/Favorite/FavoriteList.jsx
+++ src/Favorite/FavoriteList.jsx
@@ -4,6 +4,9 @@ import * as S from './FavoriteList.style';
 const FavoriteList = ({ addedFavoriteList }) => {
   const [checked, setChecked] = useState(false);
   console.log('여기는 자주가요데이터', addedFavoriteList);
+  console.log('여기는 자주가요데이터1', addedFavoriteList[0]);
+  console.log('여기는 자주가요데이터2', addedFavoriteList.martId);
+  console.log('여기는 자주가요데이터3', addedFavoriteList.data);
 
   const handleFavorite = () => {
     setChecked(prevChecked => !prevChecked);
