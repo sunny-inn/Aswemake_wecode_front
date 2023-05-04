90eb02f6417b971ee7fd95abf6b9f9f191d8174f
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-05-04 14:44:11 +0900

    modify style

--- src/Mypage/MypageComponents/ModifyInfo/ModifyPhone/ModifyPhone.style.js
+++ src/Mypage/MypageComponents/ModifyInfo/ModifyPhone/ModifyPhone.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { flex } from '../../../../Styles/Mixin';
 
 export const ModifyPhone = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 360px;
