9a66f25914f947a9fb0c052f6ce7770f7a39d0d9
HeinLee <heinlee4680@gmail.com>
2023-05-04 14:44:21 +0900

    D

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -4,35 +4,24 @@ import * as S from './Modal.style';
 const Modal = ({ handleModal, children, type }) => {
   const infoList = {
     map: MAP_MODAL_INFO,
-    logout: LOGOUT,
-    modify: MODIFY_PASSWORD_MODAL_INFO,
-    upload: UPLOAD,
+    shop: SHOP_MODAL_INFO,
   };
 
   return (
-    <S.ModalBackground>
-      <S.ModalContainer>
-        <S.ModalContentBox>
-          <S.ModalTitleBox>
-            <S.Title>{infoList[type].title}</S.Title>
-            <S.CloseImg
-              src="./images/closeImg.png"
-              onClick={handleModal}
-              alt="닫기"
-            />
-          </S.ModalTitleBox>
-          <S.ModalText>{infoList[type].message}</S.ModalText>
-          <S.ModalButtonBox>
-            {infoList[type].button[1] && (
-              <S.ModalInfoButton>{infoList[type].button[1]}</S.ModalInfoButton>
-            )}
-            <S.ModalUploadButton>
-              {infoList[type].button[0]}
-            </S.ModalUploadButton>
-          </S.ModalButtonBox>
-        </S.ModalContentBox>
-      </S.ModalContainer>
-    </S.ModalBackground>
+    <S.ModalContainer>
+      <S.ModalContentBox>
+        <span>{infoList[type].title}</span>
+        <button onClick={handleModal}>X</button>
+        <S.ModalText>{infoList[type].message}</S.ModalText>
+        <S.ModalButtonBox>
+          {infoList[type].button[1] && (
+            <S.ModalInfoButton>{infoList[type].button[1]}</S.ModalInfoButton>
+          )}
+          <S.ModalUploadButton>{infoList[type].button[0]}</S.ModalUploadButton>
+        </S.ModalButtonBox>
+      </S.ModalContentBox>
+      {/* {children} */}
+    </S.ModalContainer>
   );
 };
 
@@ -41,13 +30,13 @@ export default Modal;
 const MAP_MODAL_INFO = {
   title: '전단 등록',
   message: '전단 등록이 필요한 마트에요. 전단을 등록하러 가볼까요?',
-  button: ['마트 정보 보기', '전단 등록 하기'],
+  button: ['전단 등록 하기', '마트 등록 하기'],
 };
 
-const LOGOUT = {
-  title: '로그아웃',
-  message: '정말 로그아웃 하시겠어요?',
-  button: ['로그아웃 하기'],
+const SHOP_MODAL_INFO = {
+  title: '전단 등록',
+  message: '전단 등록이 필요한 마트에요. 전단을 등록하러 가볼까요?',
+  button: ['전단 등록 하기'],
 };
 
 const MODIFY_PASSWORD_MODAL_INFO = {
