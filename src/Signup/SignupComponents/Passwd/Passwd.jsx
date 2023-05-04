1f8d707d509aca67daa79f6bc5960b462f398152
eejungee <sulau861@gmail.com>
2023-05-04 11:09:08 +0900

    MODIFY: signup passwd 수정

--- src/Signup/SignupComponents/Passwd/Passwd.jsx
+++ src/Signup/SignupComponents/Passwd/Passwd.jsx
@@ -22,6 +22,8 @@ const Passwd = ({
           type={isPasswdEyeClicked ? 'text' : 'password'}
           placeholder="문자+숫자 8자리 이상 입력해주세요."
           onChange={handlePasswd}
+          passwd={passwd}
+          isValidPasswd={isValidPasswd}
         />
         <S.PasswdImg
           alt="eye"
@@ -33,10 +35,15 @@ const Passwd = ({
           onClick={onClickPasswdEye}
         />
       </S.PasswdInputBox>
-      {passwd !== '' && !isValidPasswd && (
+      {passwd !== '' && passwd.length < 8 && (
+        <S.AlertMsg>8자 이상 입력해주세요.</S.AlertMsg>
+      )}
+      {passwd !== '' && passwd.length >= 8 && !isValidPasswd && (
         <S.AlertMsg>비밀번호 형식이 올바르지 않습니다.</S.AlertMsg>
       )}
-      {isValidPasswd && <S.ConfirmMsg>사용가능한 비밀번호입니다.</S.ConfirmMsg>}
+      {isValidPasswd && (
+        <S.ConfirmMsg>사용 가능한 비밀번호입니다.</S.ConfirmMsg>
+      )}
       <S.PasswdInputBox>
         <S.PasswdCheckInput
           name="passwdCheck"
