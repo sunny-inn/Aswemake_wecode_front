34f0be520273debce95ef36171c1c85ec9ee3029
eejungee <sulau861@gmail.com>
2023-05-04 14:03:18 +0900

    MODIFY: search style 수정

--- src/Home/HomeComponents/Search/Search.style.js
+++ src/Home/HomeComponents/Search/Search.style.js
@@ -7,7 +7,6 @@ export const SearchBox = styled.div`
   background-color: #ffffff;
   overflow-y: hidden;
 `;
-
 export const HeaderBox = styled.div`
   ${flex('space-between', 'center', null)}
   width: 360px;
@@ -17,11 +16,9 @@ export const HeaderBox = styled.div`
   background: #ffffff;
   border-bottom: 1px solid #ececec;
 `;
-
 export const Back = styled.img`
   cursor: pointer;
 `;
-
 export const SearchBar = styled.input`
   width: 280px;
   height: 30px;
@@ -31,7 +28,7 @@ export const SearchBar = styled.input`
   font-size: 15px;
   line-height: 18px;
   color: #252525;
-
+  cursor: pointer;
   ::placeholder {
     font-weight: 500;
     font-size: 15px;
@@ -39,14 +36,12 @@ export const SearchBar = styled.input`
     color: #dbdbdb;
   }
 `;
-
 export const KeywordBox = styled.div`
   position: fixed;
   padding: 10px 20px;
   background-color: #ffffff;
   z-index: 10;
 `;
-
 export const KeywordTitle = styled.h1`
   margin-bottom: 10px;
   font-style: normal;
@@ -55,14 +50,12 @@ export const KeywordTitle = styled.h1`
   line-height: 16px;
   color: #252525;
 `;
-
 export const SearchedList = styled.ul`
   ${flex('flex-start', 'center', 'column')}
   gap: 5px;
   height: 500px;
   overflow-y: scroll;
 `;
-
 export const SearchedItem = styled.li`
   ${flex('space-between', 'center', null)}
   width: 320px;
@@ -70,8 +63,8 @@ export const SearchedItem = styled.li`
   padding: 10px;
   border: 1px solid #ececec;
   border-radius: 12px;
+  cursor: pointer;
 `;
-
 export const MartName = styled.p`
   margin-bottom: 5px;
   color: #000000;
@@ -79,21 +72,18 @@ export const MartName = styled.p`
   font-size: 17px;
   line-height: 20px;
 `;
-
 export const MartAddress = styled.p`
   color: #707070;
   font-weight: 500;
   font-size: 13px;
   line-height: 16px;
 `;
-
 export const Distance = styled.p`
   color: #707070;
   font-weight: 500;
   font-size: 13px;
   line-height: 16px;
 `;
-
 export const KeywordItem = styled.li`
   ${flex('space-between', 'center', null)}
   padding: 10px;
@@ -103,7 +93,7 @@ export const KeywordItem = styled.li`
   font-weight: 500;
   font-size: 15px;
   line-height: 18px;
-
+  cursor: pointer;
   p {
     color: #252525;
     font-weight: 500;
@@ -111,14 +101,12 @@ export const KeywordItem = styled.li`
     line-height: 18px;
   }
 `;
-
 export const DeleteBtn = styled.button`
   width: 24px;
   height: 24px;
   background-color: #ffffff;
   border-style: none;
 `;
-
 export const EmptyList = styled.div`
   ${flex('center', 'center', null)}
   height: 560px;
