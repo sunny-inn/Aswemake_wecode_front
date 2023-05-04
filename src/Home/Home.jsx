8d9da0915197f8b34c4fee6cd65a8bf1e58ab61a
HeinLee <heinlee4680@gmail.com>
2023-05-04 16:07:33 +0900

    d

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -120,23 +120,25 @@ const Home = () => {
   }, []);
 
   //여기 센터주는거
+
   useEffect(() => {
-    fetch(`https://flyers.qmarket.me/api/home/mart`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        authorization: token,
-      },
-      body: JSON.stringify({
-        y: center.lat,
-        x: center.lng,
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        setHomeMartList(data.martList);
-      });
+    center &&
+      fetch(`https://flyers.qmarket.me/api/home/mart`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+          authorization: token,
+        },
+        body: JSON.stringify({
+          y: center.lat,
+          x: center.lng,
+        }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          setHomeMartList(data.martList);
+        });
   }, [center]);
 
   useEffect(() => {
