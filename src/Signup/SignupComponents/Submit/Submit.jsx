8691f56ed8b9404108d3e064946e861b0714c50e
eejungee <sulau861@gmail.com>
2023-05-08 17:33:25 +0900

    MODIFY: signup phone 수정 중

--- src/Signup/SignupComponents/Submit/Submit.jsx
+++ src/Signup/SignupComponents/Submit/Submit.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Header from '../../../Components/Header/Header';
 import * as S from './Submit.style';
 
 const Submit = ({ setIsSubmitOpen }) => {
@@ -16,11 +17,7 @@ const Submit = ({ setIsSubmitOpen }) => {
 
   return (
     <S.SubmitBox isOpen={true} ariaHideApp={false}>
-      <S.TitleBox>
-        <img alt="arrow" src="images/signup/arrow.png" onClick={toSignup} />
-        <h1>회원가입 완료</h1>
-        <div />
-      </S.TitleBox>
+      <Header type="submit" />
       <S.MsgBox>
         <S.SubmitMsg>축하드립니다!</S.SubmitMsg>
         <S.SubmitMsg>회원가입이 완료되었습니다.</S.SubmitMsg>
