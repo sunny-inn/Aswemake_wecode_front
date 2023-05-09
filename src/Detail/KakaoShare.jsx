ca93ae18d1c46d23c5e856403bdd4419869c89b4
HeinLee <heinlee4680@gmail.com>
2023-05-09 16:06:07 +0900

    share

--- src/Detail/KakaoShare.jsx
+++ src/Detail/KakaoShare.jsx
@@ -1,4 +1,4 @@
-// const APP_JAVA_KEY = process.env.REACT_APP_JAVA_SCRIPT_KEY;
+const APP_JAVA_KEY = process.env.REACT_APP_JAVA_SCRIPT_KEY;
 const KakaoShare = (url, title, argumentKey, detailMartList) => {
   if (window.Kakao) {
     const kakao = window.Kakao;
@@ -8,7 +8,7 @@ const KakaoShare = (url, title, argumentKey, detailMartList) => {
 
     console.log('마트리스트?잘나와', detailMartList[0]);
 
-    // console.log('키나오나', APP_JAVA_KEY);
+    console.log('키나오나', APP_JAVA_KEY);
     kakao.Link.sendDefault({
       objectType: 'feed',
       content: {
