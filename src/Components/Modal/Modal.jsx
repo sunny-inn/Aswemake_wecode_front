211573fde20c3304a80d5cbcefb2cbeac0978723
HeinLee <heinlee4680@gmail.com>
2023-05-10 08:30:48 +0900

    d

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -41,16 +41,16 @@ const MAP_MODAL_INFO = {
   title: '전단 등록',
   close: true,
   message1:
-    '전단등록이 필요한 마트에요.                 전단을 등록하러 가볼까요?',
+    '전단등록이 필요한 마트에요.                 &nbsp; 전단을 등록하러 가볼까요?',
   button: ['전단 등록하기'],
 };
 
-// const MODIFY_PASSWORD_MODAL_INFO = {
-//   title: '비밀번호 불일치',
-//   close: false,
-//   message1: '비밀번호가 일치하지 않습니다. 다시 확인해주세요.',
-//   button: ['확인'],
-// };
+const MODIFY_PASSWORD_MODAL_INFO = {
+  title: '비밀번호 불일치',
+  close: false,
+  message1: '비밀번호가 일치하지 않습니다. 다시 확인해주세요.',
+  button: ['확인'],
+};
 
 const UPLOAD = {
   title: '전단등록 요청 완료',
