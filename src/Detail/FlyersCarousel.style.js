933a13702750b370c23cf0b4a51338fc715ee1fd
HeinLee <heinlee4680@gmail.com>
2023-05-10 11:48:20 +0900

    d

--- src/Detail/FlyersCarousel.style.js
+++ src/Detail/FlyersCarousel.style.js
@@ -16,6 +16,9 @@ export const FlyerCarouselContainer = styled.div`
   .slick-slide {
     margin-right: 10px;
   }
+  .slick-track {
+    display: flex;
+  }
 `;
 
 export const FlyerImage = styled.img`
