01dcbb97d4322d6907bae13bbe6620447ba9b339
HeinLee <heinlee4680@gmail.com>
2023-05-11 02:25:33 +0900

    d

--- src/Favorite/Favorite.jsx
+++ src/Favorite/Favorite.jsx
@@ -1,3 +1,57 @@
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import FavoriteEmpty from './FavoriteEmpty';
+// import FavoriteList from './FavoriteList';
+// import Header from '../Components/Header/Header';
+
+// const Favorite = () => {
+//   const [addedFavoriteList, setAddedFavoriteList] = useState([]);
+//   const [imageStates, setImageStates] = useState(
+//     Array(addedFavoriteList.length).fill(false)
+//   );
+
+//   console.log('이거가없다면?', addedFavoriteList);
+//   const token = localStorage.getItem('token');
+//   useEffect(() => {
+//     fetch('https://flyers.qmarket.me/api/favorite', {
+//       method: 'GET',
+//       credentials: 'include',
+//       headers: {
+//         'Content-Type': 'application/json;charset=utf-8',
+//         authorization: token,
+//       },
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         setAddedFavoriteList(data.data);
+//         fetch('https://flyers.qmarket.me/api/favorite', {
+//       method: 'GET',
+//       credentials: 'include',
+//       headers: {
+//         'Content-Type': 'application/json;charset=utf-8',
+//         authorization: token,
+//       },
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <Header type="favorite" />
+//       {addedFavoriteList.length === 0 ? (
+//         <FavoriteEmpty />
+//       ) : (
+//         <FavoriteList
+//           addedFavoriteList={addedFavoriteList}
+//           setImageStates={setImageStates}
+//           imageStates={imageStates}
+//           setAddedFavoriteList={setAddedFavoriteList}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Favorite;
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import FavoriteEmpty from './FavoriteEmpty';
@@ -6,12 +60,10 @@ import Header from '../Components/Header/Header';
 
 const Favorite = () => {
   const [addedFavoriteList, setAddedFavoriteList] = useState([]);
-  const [imageStates, setImageStates] = useState(
-    Array(addedFavoriteList.length).fill(false)
-  );
+  const [imageStates, setImageStates] = useState([]);
 
-  console.log('이거가없다면?', addedFavoriteList);
   const token = localStorage.getItem('token');
+
   useEffect(() => {
     fetch('https://flyers.qmarket.me/api/favorite', {
       method: 'GET',
@@ -24,8 +76,9 @@ const Favorite = () => {
       .then(response => response.json())
       .then(data => {
         setAddedFavoriteList(data.data);
+        setImageStates(Array(data.data.length).fill(false));
       });
-  }, []);
+  }, [token]);
 
   return (
     <div>
