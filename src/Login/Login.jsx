62e64b779b2044f9d90569c063739147fad4699a
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-05-11 01:29:38 +0900

    wip

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -104,45 +104,6 @@ const Login = () => {
       });
   };
 
-  useEffect(() => {
-    const instance = axios.create({
-      baseURL: 'https://flyers.qmarket.me',
-    });
-
-    instance.interceptors.request.use(async function (config) {
-      const accessToken = localStorage.getItem('token');
-      config.headers['Authorization'] = `Bearer ${accessToken}`;
-
-      return config;
-    });
-
-    instance.interceptors.response.use(function (response) {
-      if (response.data.message === 'CREATED NEW ACCESS TOKEN') {
-        axios({
-          url: 'https://flyers.qmarket.me/api/users/login',
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-          },
-        })
-          .then(response => {
-            localStorage.setItem('token', response.data.accessToken);
-            instance.defaults.headers.common['Authorization'] =
-              'Bearer' + response.data.accessToken;
-          })
-          .then(response => {
-            window.location.reload();
-          });
-      } else if (response.data.message === 'YOU NEED LOGIN AGAIN') {
-        localStorage.clear();
-        window.alert('로그인 기한이 만료되어 자동으로 로그아웃 되었습니다.');
-        navigate('/');
-      }
-
-      return response;
-    });
-  }, [navigate]);
-
   // useEffect(() => {
   //   if (localStorage.getItem('token')) {
   //     navigate('/home');
--- src/Login/TokenRefresher.jsx
+++ src/Login/TokenRefresher.jsx
@@ -7,45 +7,68 @@ const TokenRefresher = () => {
 
   const instance = axios.create({
     baseURL: 'https://flyers.qmarket.me',
+    withCredentials: true,
   });
 
-  instance.interceptors.request.use(function (config) {
+  instance.interceptors.request.use(config => {
     const accessToken = localStorage.getItem('token');
 
-    config.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
 
     return config;
   });
 
-  instance.interceptors.response.use(function (response) {
-    console.log(1, response);
+  instance.interceptors.response.use(
+    response => {
+      return response;
+    },
+    async error => {
+      const originalRequest = error.config;
 
-    if (response.data.message === 'CREATED NEW ACCESS TOKEN') {
-      axios({
-        url: 'https://flyers.qmarket.me/api/users/login',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-        },
-      })
-        .then(response => {
-          localStorage.setItem('token', response.data.accessToken);
-          instance.defaults.headers.common['Authorization'] =
-            'Bearer' + response.data.accessToken;
-        })
-        .then(response => {
-          window.location.reload();
-        });
-    } else if (response.data.message === 'YOU NEED LOGIN AGAIN') {
-      localStorage.clear();
-      window.alert('로그인 기한이 만료되어 자동으로 로그아웃 되었습니다.');
-      navigate('/');
+      if (error.response.data.message === 'CREATED NEW ACCESS TOKEN') {
+        try {
+          const response = await instance.post(
+            'https://flyers.qmarket.me/api/users/login'
+          );
+          const token = response.data.accessToken;
+          localStorage.setItem('token', token);
+
+          return instance(originalRequest);
+        } catch (e) {
+          localStorage.removeItem('token');
+          navigate('/');
+        }
+      }
+      return Promise.reject(error);
     }
+    // if (response.data.message === 'CREATED NEW ACCESS TOKEN') {
+    //   axios({
+    //     url: 'https://flyers.qmarket.me/api/users/login',
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json;charset=utf-8',
+    //     },
+    //   })
+    //     .then(response => {
+    //       localStorage.setItem('token', response.data.accessToken);
+    //       instance.defaults.headers.common['Authorization'] =
+    //         'Bearer' + response.data.accessToken;
+    //     })
+    //     .then(response => {
+    //       window.location.reload();
+    //     });
+    // } else if (response.data.message === 'YOU NEED LOGIN AGAIN') {
+    //   localStorage.clear();
+    //   window.alert('로그인 기한이 만료되어 자동으로 로그아웃 되었습니다.');
+    //   navigate('/');
+    // }
 
-    console.log(2, response);
+    // console.log(2, response);
 
-    return response;
-  });
+    // return response;
+  );
 
   return null;
 };
