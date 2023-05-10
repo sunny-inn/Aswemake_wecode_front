9d9175adc10536c4a53357c37b0492dea3a95b7e
eejungee <sulau861@gmail.com>
2023-05-10 15:33:25 +0900

    FIX: home 지도 수정 중

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -251,7 +251,7 @@ const Home = () => {
             zoomControl={false}
             centerPoint={centerPoint}
           >
-            {homeMartList[0] &&
+            {homeMartList[0] ? (
               homeMartList.map((mart, index) => {
                 //2일전계산
                 const now = new Date();
@@ -280,7 +280,10 @@ const Home = () => {
                     onClick={e => handleMarkerClick(e, mart, index)}
                   />
                 );
-              })}
+              })
+            ) : (
+              <div />
+            )}
             <S.SearchBox>
               <div onClick={handleSearch}>
                 <S.SearchBar
