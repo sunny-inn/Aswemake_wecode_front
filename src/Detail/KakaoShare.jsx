e4a29dfb201d287b1a5c9d5a2ff988d63ad632c7
HeinLee <heinlee4680@gmail.com>
2023-05-10 16:26:09 +0900

    d

--- src/Detail/KakaoShare.jsx
+++ src/Detail/KakaoShare.jsx
@@ -1,7 +1,6 @@
 const APP_JAVA_KEY = 'a320e86acb93342abb996dc9556d6c55';
 
 const KakaoShare = (url, title, argumentKey, detailMartList) => {
-  console.log('공유하기', detailMartList);
   if (window.Kakao) {
     const kakao = window.Kakao;
     if (!kakao.isInitialized()) {
