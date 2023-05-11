edb8c4abdbb3f2cd38fc578aa068e5f1ba1fec6d
eejungee <sulau861@gmail.com>
2023-05-11 18:23:55 +0900

    FIX: style 수정 중

--- src/Home/CarouselContent.jsx
+++ src/Home/CarouselContent.jsx
@@ -38,7 +38,7 @@ const CarouselContent = ({ mart, onClickMartItem }) => {
           <div>
             <S.CarouselImg
               src={
-                isClicked === '0'
+                mart.martFlyerImages === '0'
                   ? 'images/flyernone.png'
                   : mart.martFlyerImages[0].imageUrl
               }
