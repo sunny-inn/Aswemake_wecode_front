e1ae23e9da261789fd4d55f2b5b526dd8d63063d
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-05-11 15:12:28 +0900

    wip

--- src/Favorite/Favorite.jsx
+++ src/Favorite/Favorite.jsx
@@ -33,7 +33,7 @@ const Favorite = () => {
   return (
     <div>
       <Header type="favorite" />
-      {addedFavoriteList.length === 0 ? (
+      {addedFavoriteList[0].length === 0 ? (
         <FavoriteEmpty />
       ) : (
         <FavoriteList
