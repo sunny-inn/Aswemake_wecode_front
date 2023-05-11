c9aa6c54e8e0f2068f95fa19562ba21b17a984f7
minkyung <minngaeng@gmail.com>
2023-05-11 10:19:57 +0900

    mdf

--- src/WithdrawPoint/SetPoint.jsx
+++ src/WithdrawPoint/SetPoint.jsx
@@ -42,7 +42,13 @@ const SetPoint = () => {
             {formatNumber(withdrawalPoints)}원
           </S.Inform>
           <S.InformPoint>인출 요청이 완료되었습니다!</S.InformPoint>
+          <S.WithdrawNotify>
+            인출 요청 후 7일 이내 등록된 계좌로 입금됩니다.
+          </S.WithdrawNotify>
         </S.Container>
+        <Link to="/mypage">
+          <S.FinBtn>확인</S.FinBtn>
+        </Link>
       </LoginLayout>
     </>
   );
