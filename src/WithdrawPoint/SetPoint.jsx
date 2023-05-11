0daa18c5ae941a3f9fa078b2d633c7148015edfc
minkyung <minngaeng@gmail.com>
2023-05-11 10:10:11 +0900

    mdf

--- src/WithdrawPoint/SetPoint.jsx
+++ src/WithdrawPoint/SetPoint.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import * as S from './SetPoint.style';
-import Header from '../Components/Header/Header';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import LoginLayout from '../Login/Component/LoginLayout';
+import { Link, useLocation } from 'react-router-dom';
 
 const SetPoint = () => {
   const location = useLocation();
@@ -10,7 +8,7 @@ const SetPoint = () => {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   };
 
-  const accountHolderName = location.state?.accountHolderName || '예금주명';
+  const accountHolderName = location.state?.accountHolderName || '';
   const withdrawalPoints = location.state?.withdrawalPoints
     ? parseInt(location.state.withdrawalPoints, 10)
     : 0;
@@ -21,21 +19,19 @@ const SetPoint = () => {
 
   return (
     <div>
-      <LoginLayout>
-        <S.Container>
-          <S.Name>{accountHolderName}님</S.Name>
-          <S.Inform style={{ color: '#FF6A21' }}>
-            &nbsp;&nbsp;{formatNumber(withdrawalPoints)}원
-          </S.Inform>
-          <S.InformPoint>&nbsp;&nbsp;인출 요청이 완료되었습니다!</S.InformPoint>
-          <S.WithdrawNotify>
-            인출 요청 후 7일 이내 등록된 계좌로 입금됩니다.
-          </S.WithdrawNotify>
-        </S.Container>
-        <Link to="/mypage">
-          <S.FinBtn>확인</S.FinBtn>
-        </Link>
-      </LoginLayout>
+      <S.Container>
+        <S.Name>{accountHolderName}님</S.Name>
+        <S.Inform style={{ color: '#FF6A21' }}>
+          &nbsp;&nbsp;{formatNumber(withdrawalPoints)}원
+        </S.Inform>
+        <S.InformPoint>&nbsp;&nbsp;인출 요청이 완료되었습니다!</S.InformPoint>
+        <S.WithdrawNotify>
+          인출 요청 후 7일 이내 등록된 계좌로 입금됩니다.
+        </S.WithdrawNotify>
+      </S.Container>
+      <Link to="/mypage">
+        <S.FinBtn>확인</S.FinBtn>
+      </Link>
     </div>
   );
 };
